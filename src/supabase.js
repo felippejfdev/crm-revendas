fb994a132db24225b832a1fff097b3cd987aac05
felippe <felippejf411@gmail.com>
2026-05-23 06:22:58 -0300

    commit

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const SUPABASE_URL = "https://rpwwvospxaefisiuppoo.supabase.co"
-const SUPABASE_KEY = "sb_publishable_5PTxJzrqd4H5AJ2-VFTIgg_qZxKKvVI"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwd3d2b3NweGFlZmlzaXVwcG9vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNDA0NjEsImV4cCI6MjA5NDcxNjQ2MX0.1nOUuIJWgDGomYVP_IahzY2NV3fHfO8HiFbftoR6LG0"
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
